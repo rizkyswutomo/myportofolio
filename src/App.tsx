70c95ca7c8c9df2b1d7ebc2675bbb033e036cc87
rizkyswutomo <rizkyswutomo@gmail.com>
2025-08-12 14:48:06 +0700

    Change navigation text to Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -154,7 +154,7 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
             <div className="text-2xl font-bold text-blue-900">
-              Rizky Setyaji Wahyu Utomo
+              Portofolio
             </div>
             
             {/* Desktop Navigation */}
